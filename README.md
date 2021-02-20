0f4a66e66c44d08df34c0eda25bcc79e9fbff9ec
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-02-19 22:42:07 -0500

    functionality is complete, README is updated

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 ## The Objective
 
+Using HTML, CSS, and Javascript, I have created a timed coding quiz with multiple-choice questions. When the user answers questions correctly their score goes up, and when they get a question wrong time is taken away from the timer. After they have answered all questions, their final score is shown and they are given the option to enter their Initials & score into the highscore list. This entry is saved into Local Storage until it is cleared. 
+
 ## The Process
+
+The HTML contains multiple sections - including a question container, intro container, end screen container, and highscores page container. These sections are revealed and hidden by adding and removing a class of "hide." When the user selects "Start Quiz," the first question is revealed and the timer begins to count down from 60. When the user selects an answer, the next question is revealed. Based on which answer the user selects, the screen will show "correct!" or "wrong!" underneath the question container. 
+
+On the Highscores Page, the user has the choice to select "Go Back" or "Clear Highscores." If they click "Go Back," they will be brought to the beginning of the quiz and the timer will reset. "Clear Highscores" will empty the list of highscores shown on the screen as well as the Local Storage. 
 
 ## Link to Deployed Project 
 
+
+
 ## Demonstration 
+
+As you can see from the demonstration, there are a few bugs that I was not able to work out. If you click "clear Highscores" and then click on "View Highscores," the scores that were supposed to be cleared will show up. Also, if you click "View Highscores" when you are on the Highscores page, the list will render again. Besides that, it is working as expected. 
+
+![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./assets/API-Code-Quiz.gif)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declare Variables 
+// Arrangement
 const startButton = document.querySelector("#start-btn"); 
 const introScreen = document.querySelector("#intro-container"); 
 const questionContainer = document.querySelector("#question-container"); 
@@ -8,14 +8,31 @@ const Result = document.querySelector("#rightOrWrong");
 const endScreen = document.querySelector("#end-screen-container"); 
 const scoreOnEndScreen = document.querySelector("#final-score"); 
 const timerEl = document.querySelector("#countdown"); 
+const submitBtnEl = document.querySelector("#submit-btn"); 
+const userInitialsInput = document.querySelector("#enter-initials"); 
+const highscoresPage = document.querySelector("#highscores-page"); 
+const highscoresList = document.querySelector("#entered-scores"); 
+const goBackButton = document.querySelector("#go-back-btn");
+const clearHighscoresButton = document.querySelector("#clear-highscores-btn"); 
+const viewHighscores = document.querySelector("#view-high-scores"); 
 
 var currentQuestion; 
 var currentQuestionIndex; 
 
 var score = 0; 
 var timeLeft = 59; 
+var timeInterval; 
 
+if (localStorage.length === 0) {
+    var highscores = []; 
+} else {
+    var highscores = JSON.parse(localStorage.getItem("highscores"));  
+} 
 
+var highscore; 
+var li; 
+var storedHighscores; 
+var highscoreText; 
 
 //Array of Questions w/ Answers
 const questions = [
@@ -81,8 +98,9 @@ function startQuiz() {
     setNextQuestion(); 
 }
 
+// the countdown function (AKA: the timer) starts when the startQuiz function is called
 function countdown() {
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = "Time Left: " + timeLeft + " seconds";
             timeLeft--;
@@ -95,6 +113,8 @@ function countdown() {
     }, 1000); 
 }
 
+// brings up the first question after the "Start Quiz" button is pressed, and continues to show questions until no questions are left in the array
+// when no questions are left, the End Screen will show 
 function setNextQuestion() {
     resetState();
     if (currentQuestion.length < currentQuestionIndex + 1) {
@@ -104,6 +124,8 @@ function setNextQuestion() {
     }
 }
 
+// sets the content of the question & answer fields and shows that to the user 
+// when one of the answers is selected, it calls the selectAnswer function
 function showQuestion(question) {
     questionElement.innerText = question.question; 
     question.answers.forEach(answer => {
@@ -118,6 +140,7 @@ function showQuestion(question) {
     })
 }
 
+// this function registers whether the selected answer is right or wrong, and either adds to the score and takes away from the time respectively 
 function selectAnswer(event) {
     const selectedButton = event.target; 
     if (selectedButton.dataset.correct) {
@@ -131,15 +154,16 @@ function selectAnswer(event) {
     setNextQuestion();
     
 }
-
+// starts each question with a clean slate by removing the question that came before
 function resetState() {
     while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild)
+        answerButtons.removeChild(answerButtons.firstChild);
     }
 }
-
+// brings up the End Screen, informing the user they have completed the quiz
+// shows the user their score and allows them to enter their initials
 function showEndScreen () {
-    timerEl.classList.add("hide"); 
+    clearInterval(timeInterval); 
     questionContainer.classList.add("hide");
     endScreen.classList.remove("hide"); 
     const showScore = document.createElement("p");
@@ -148,7 +172,72 @@ function showEndScreen () {
 
 }
 
+// functions related to Local Storage & showing the Highscores Page after user clicks "submit"
+submitBtnEl.addEventListener("click", showHighscoresPage);
 
+function showHighscoresPage (event) {
+    event.preventDefault();  
+    endScreen.classList.add("hide"); 
+    highscoresPage.classList.remove("hide"); 
 
+    highscoreText = userInitialsInput.value + " - " + (score + timeLeft); 
 
+    if (userInitialsInput.value !== "") {
+    highscores.push(highscoreText); 
+    userInitialsInput.value = ""; 
+    }
 
+    storeHighscores();
+    renderHighscores(); 
+
+}
+function storeHighscores() {
+    localStorage.setItem("highscores", JSON.stringify(highscores)); 
+}
+function renderHighscores() {
+    for (var i = 0; i < 10; i++) {
+        highscore = highscores[i];
+
+        li = document.createElement("li");
+        li.textContent = highscore;
+        li.setAttribute("data-index", i);
+
+        highscoresList.appendChild(li); 
+    }
+} 
+
+//Go Buck Button - returns you to the beginning of the quiz 
+goBackButton.addEventListener("click", function() {
+    introScreen.classList.remove("hide"); 
+    highscoresPage.classList.add("hide"); 
+
+    while (scoreOnEndScreen.firstChild) {
+        scoreOnEndScreen.removeChild(scoreOnEndScreen.firstChild);
+    }
+
+    while (highscoresList.firstChild) {
+        highscoresList.removeChild(highscoresList.firstChild);
+    }
+    timeLeft = 60;
+    timerEl.textContent = "Time Left: " + timeLeft + " seconds";
+
+    
+})
+
+// Clear Highscores button - removes items from Local Storage 
+clearHighscoresButton.addEventListener("click", function() {
+    localStorage.clear(); 
+    while (highscoresList.firstChild) {
+    highscoresList.removeChild(highscoresList.firstChild);
+    } 
+})
+
+//"View High Scores" link 
+viewHighscores.addEventListener("click", function(event) {
+    showHighscoresPage(event); 
+    introScreen.classList.add("hide"); 
+    questionContainer.classList.add("hide");
+    endScreen.classList.add("hide"); 
+    clearInterval(timeInterval); 
+
+});
